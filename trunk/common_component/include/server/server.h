a08bfc8de65ea9c3c51f8f4cbe88b7ec4f13a032
eyjian <eyjian@e6536627-aa60-2b80-6d29-bd3d1986124f>
2012-07-20 16:04:13 +0000

    还原

--- trunk/common_component/include/server/server.h
+++ trunk/common_component/include/server/server.h
@@ -18,36 +18,34 @@
  */
 #ifndef MOOON_SERVER_H
 #define MOOON_SERVER_H
+#include <server/factory.h>
 #include <server/connection.h>
 #include <server/packet_handler.h>
 #include <server/thread_follower.h>
 SERVER_NAMESPACE_BEGIN
 
-/***
-  * Server组件接口，是一个抽象接口，所以必须实现它
-  */
-class IServer
-{
-public:
-    virtual ~IServer() {}
-
-    /** 创建包处理器 */
-    virtual IPacketHandler* create_packet_handler(IConnection* connection) = 0;
-
-    /** 创建线程伙伴 */
-    virtual IThreadFollower* create_thread_follower(uint16_t index)
-    {
-        return NULL;
-    }
-};
-
-////////////////////////////////////////////////////////////////////////////////
+typedef void* server_t;
 
 /**
   * 日志器，所有实例共享同一个日志器
   * 如需要记录日志，则在调用create_server之前应当设置好日志器
   */
 extern sys::ILogger* logger;
+
+//////////////////////////////////////////////////////////////////////////
+
+/**
+  * 销毁Server
+  */
+extern void destroy(server_t server);
+
+/***
+  * 创建Server
+  * @config: Server配置
+  * @factory: Server工厂
+  * @return 如果创建失败返回NULL，否则返回非NULL
+  */
+extern server_t create(server::IConfig* config, server::IFactory* factory);
 
 SERVER_NAMESPACE_END
 #endif // MOOON_SERVER_H
